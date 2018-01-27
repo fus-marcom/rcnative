4e8434f90462e112ca444034065d36022c78124d
Angel Rodriguez <erodriguezfeliz@gmail.com>
2018-01-26 22:06:07 -0400

    feact: modified about page style and create  FlatBanner component

--- app/components/FlatBanner.js
+++ app/components/FlatBanner.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-const capSplitFirstLetter = title => {}
+const capSplitFirstLetter = title => {
+  if (title !== ' ' && title.length >= 1) {
+    var word
+    word = title.substring(0, 1)
+    return word.toUpperCase()
+  }
+}
 
 const FlatBanner = props => (
   <View>
